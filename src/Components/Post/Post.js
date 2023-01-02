512ab2c2bae6487de8c9923eb831ab84887bf787
krutr <krutrupera74@gmail.com>
2023-01-02 10:36:23 +0530

    First Commit

---
+++ src/Components/Post/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Post.css';
+
+const post = (props) => (
+    <article className='Post'>
+        <h1>
+            <div className='Info'>
+                <div className='Author'>
+                    Author
+                </div>
+            </div>
+        </h1>
+    </article>
+)
+
+export default post;
